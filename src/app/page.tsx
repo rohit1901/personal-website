0631487b6fd8f23819db917437dd95d1638429dc
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-09 09:42:33 +0200

    CHORE: TODOs added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,17 @@ import {Carousel} from "@website/components/Carousel";
 import {Runner} from "@website/components/Runner";
 import {Footer} from "@website/components/Footer";
 import {MainContent} from "@website/components/MainContent";
-
+// TODO: Currently reading page
+// TODO: Projects page
+// TODO: Add Book reviews to substack
+// TODO: Responsive design
+// TODO: Download Resume
+// TODO: Icons
+// TODO: Upload Assets to S3 and consume via CloudFront
+// TODO: MongoDB config and APIs
+// TODO: Consume Literal.club GraphQL API for Currently Reading Books and other statistics
+// TODO: Readme
+// TODO: CI/CD, Heroku, GitHub Actions
 export default async function Home() {
     return (
         <main className="min-w-screen min-h-screen flex-auto">
