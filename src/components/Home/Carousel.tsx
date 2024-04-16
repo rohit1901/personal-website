06586d92f11cd1166b122f1773cf95a339fdfef7
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-16 13:49:45 +0200

    FIX: removed next/Image

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from "next/image";
 import {useEffect, useState} from "react";
 import {InstagramMedia} from "@website/types";
 import {ContentLoader} from "@website/components/ContentLoader";
@@ -23,7 +22,7 @@ export const Carousel = () => {
         <div className="carousel rounded-box p-4 space-x-4 bg-transparent justify-center">
             {media?.map((m, i) => (
                 <div className="carousel-item mr-2" key={m.media_url}>
-                    <Image src={m.media_url} alt={`instagram profile image-${i}`} className="object-cover rounded-lg" loading="lazy"
+                    <img src={m.media_url} alt={`instagram profile image-${i}`} className="object-cover rounded-lg" loading="lazy"
                            width={300} height={400}/>
                 </div>
             ))}
