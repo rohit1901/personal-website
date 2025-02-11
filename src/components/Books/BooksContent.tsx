e06279c7be436283e784fc970c6b062621312ce5
Rohit Khanduri <rohit.khanduri@hotmail.com>
2025-02-11 15:57:37 +0100

    feat: goodreads feed
    now consuming goodreads feed instead of Literal

--- src/components/Books/BooksContent.tsx
+++ src/components/Books/BooksContent.tsx
@@ -1,25 +1,33 @@
-import {LiteralReadingState} from "@website/types";
-import {getBooks, getLiteralReadingStatusValues} from "@website/lib";
-import {Book} from "@website/components/Books/Book";
+import { Book } from "@website/components/Books/Book";
 import Section from "@website/components/Section";
-import {getLiteralReadingStates} from "@website/lib/fetchData";
+import { getBooks, getLiteralReadingStatusValues } from "@website/lib";
+import { getGoodreadsBooks } from "@website/lib/fetchData";
+import { LiteralReadingState } from "@website/types";
 
 export default async function BooksContent() {
-    const readingStates: LiteralReadingState[] = await getLiteralReadingStates();
-    return (
-        <Section className="flex-col">
-            <div className="flex flex-col xl:w-3/4 2xl:w-3/4 w-full mb-10">
-                <h1 className="text-4xl font-bold tracking-tight w-3/4">Books that I&apos;m reading, I&apos;ve read, want to read, and a lot of other
-                    things
-                    related to books.</h1>
-                <div className="mt-6 space-y-7 text-base">
-                    <span>Here are some of the books that I&apos;ve read, am reading, or want to read.
-                        I&apos;m always looking for new books to read, so if you have any recommendations, feel free to reach out to me.</span>
-                </div>
-            </div>
-            {getLiteralReadingStatusValues().map(status => (
-                <Book key={status} books={getBooks(readingStates, status)} status={status}/>
-            ))}
-        </Section>
-    )
+  const readingStates: LiteralReadingState[] = await getGoodreadsBooks();
+  return (
+    <Section className="flex-col">
+      <div className="flex flex-col xl:w-3/4 2xl:w-3/4 w-full mb-10">
+        <h1 className="text-4xl font-bold tracking-tight w-3/4">
+          Books that I&apos;m reading, I&apos;ve read, want to read, and a lot
+          of other things related to books.
+        </h1>
+        <div className="mt-6 space-y-7 text-base">
+          <span>
+            Here are some of the books that I&apos;ve read, am reading, or want
+            to read. I&apos;m always looking for new books to read, so if you
+            have any recommendations, feel free to reach out to me.
+          </span>
+        </div>
+      </div>
+      {getLiteralReadingStatusValues().map((status) => (
+        <Book
+          key={status}
+          books={getBooks(readingStates, status)}
+          status={status}
+        />
+      ))}
+    </Section>
+  );
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,14 +1,23 @@
-import {DEFAULT_LOCALE, FALLBACK_IMAGE, INSTAGRAM_MEDIA_URL} from "@website/constants";
-import {DocumentNode} from "graphql";
-import {InstagramError, LiteralBook, LiteralReadingState, LiteralReadingStatus} from "@website/types";
-import {SubstackItem} from "substack-feed-api";
+import {
+  DEFAULT_LOCALE,
+  FALLBACK_IMAGE,
+  INSTAGRAM_MEDIA_URL,
+} from "@website/constants";
+import {
+  InstagramError,
+  LiteralBook,
+  LiteralReadingState,
+  LiteralReadingStatus,
+} from "@website/types";
+import { DocumentNode } from "graphql";
+import { SubstackItem } from "substack-feed-api";
 
 export const getUserLocale = (): string => {
-    if (typeof window !== "undefined") {
-        return window.navigator.language
-    }
-    return DEFAULT_LOCALE
-}
+  if (typeof window !== "undefined") {
+    return window.navigator.language;
+  }
+  return DEFAULT_LOCALE;
+};
 
 /**
  * This function formats the date to a human-readable format using the user's locale.
@@ -16,13 +25,13 @@ export const getUserLocale = (): string => {
  * @returns {string} - the formatted date
  */
 export const formatDate = (date: string): string => {
-    const userLocale = getUserLocale()
-    return new Date(date).toLocaleDateString(userLocale, {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    })
-}
+  const userLocale = getUserLocale();
+  return new Date(date).toLocaleDateString(userLocale, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
 /**
  * This function formats the date to a human-readable format using the user's locale.
  * @param fromDate {string | undefined} - the from date to format
@@ -31,18 +40,20 @@ export const formatDate = (date: string): string => {
  * @example getMMYYYYDate("01-2021", "12-2021") => "January 2021 - December 2021"
  */
 export const getMMYYYYDate = (fromDate?: string, toDate?: string): string => {
-    if (!fromDate) return ""
-    const userLocale = getUserLocale()
-    const from = new Date(`01-${fromDate}`).toLocaleDateString(userLocale, {
-        month: 'short',
-        year: 'numeric'
-    })
-    const to = toDate ? new Date(`01-${toDate}`).toLocaleDateString(userLocale, {
-        month: 'short',
-        year: 'numeric'
-    }) : "Present"
-    return `${from} - ${to}`
-}
+  if (!fromDate) return "";
+  const userLocale = getUserLocale();
+  const from = new Date(`01-${fromDate}`).toLocaleDateString(userLocale, {
+    month: "short",
+    year: "numeric",
+  });
+  const to = toDate
+    ? new Date(`01-${toDate}`).toLocaleDateString(userLocale, {
+        month: "short",
+        year: "numeric",
+      })
+    : "Present";
+  return `${from} - ${to}`;
+};
 
 /**
  * This function sets the theme attribute on the runners.
@@ -50,28 +61,28 @@ export const getMMYYYYDate = (fromDate?: string, toDate?: string): string => {
  * @returns {void}
  */
 export const handleRunnerTheme = (t: string): void => {
-    const runnerTheme = t === "black" ? "dark" : "light";
-    const runners = document.getElementsByClassName("runner")
-    Array.from(runners).forEach((runner) => {
-        runner.setAttribute("data-theme", runnerTheme)
-    });
-    document.getElementById("footer")?.setAttribute("data-theme", runnerTheme)
-}
+  const runnerTheme = t === "black" ? "dark" : "light";
+  const runners = document.getElementsByClassName("runner");
+  Array.from(runners).forEach((runner) => {
+    runner.setAttribute("data-theme", runnerTheme);
+  });
+  document.getElementById("footer")?.setAttribute("data-theme", runnerTheme);
+};
 /**
  * This function sets the theme attribute on the html element and the substack embed
  * @param t {string} - the theme to set
  * @returns {void}
  */
 export const handleTheme = (t: string): void => {
-    // set the theme attribute on the html element
-    Array.from(document.getElementsByTagName("html")).forEach((html) => {
-        html.setAttribute("data-theme", t)
-    });
-    // set the theme attribute on the substack embed
-    document.getElementById("substack-embed")?.setAttribute("data-theme", t)
-    // set the theme attribute on the runners
-    handleRunnerTheme(t)
-}
+  // set the theme attribute on the html element
+  Array.from(document.getElementsByTagName("html")).forEach((html) => {
+    html.setAttribute("data-theme", t);
+  });
+  // set the theme attribute on the substack embed
+  document.getElementById("substack-embed")?.setAttribute("data-theme", t);
+  // set the theme attribute on the runners
+  handleRunnerTheme(t);
+};
 /**
  * Get the image url from the src
  * Fetch from cloudfront in production and from dev in development
@@ -80,73 +91,84 @@ export const handleTheme = (t: string): void => {
  * @param src {string} - the src of the image
  */
 export const getImageUrl = (src?: string) => {
-    if (!src) {
-        return FALLBACK_IMAGE;
-    }
-    const basePath = process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL;
-    return `${basePath}${src}`
-}
+  if (!src) {
+    return FALLBACK_IMAGE;
+  }
+  const basePath = process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL;
+  return `${basePath}${src}`;
+};
 /**
  * Returns the complete Instagram URL
  * @returns {string} - the Instagram URL
  */
 export const getInstagramUrl = (): string => {
-    const ACCESS_TOKEN = process.env.INSTAGRAM_TESTER_SECRET;
-    return `${INSTAGRAM_MEDIA_URL}${encodeURIComponent(ACCESS_TOKEN)}`;
-}
+  const ACCESS_TOKEN = process.env.INSTAGRAM_TESTER_SECRET;
+  return `${INSTAGRAM_MEDIA_URL}${encodeURIComponent(ACCESS_TOKEN)}`;
+};
 /**
  * Get the GraphQL query string
  * @param taggedQuery {DocumentNode} - the tagged query [GraphQL query string with the gql tag]
  * @param variables {any} - the variables to pass to the query
  * @returns {string} - the GraphQL query string
  */
-export const getGraphQLQueryStr = (taggedQuery: DocumentNode, variables?: any): string => {
-    return JSON.stringify({query: taggedQuery.loc?.source.body, variables})
-}
+export const getGraphQLQueryStr = (
+  taggedQuery: DocumentNode,
+  variables?: any,
+): string => {
+  return JSON.stringify({ query: taggedQuery.loc?.source.body, variables });
+};
 /**
  * Get the reading section heading based on the status
  * @returns {Array<LiteralReadingStatus>} - the reading status values
  */
-export const getLiteralReadingStatusValues = (): Array<LiteralReadingStatus> => {
-    return ["IS_READING", "FINISHED", "WANTS_TO_READ"]
-}
+export const getLiteralReadingStatusValues =
+  (): Array<LiteralReadingStatus> => {
+    return ["IS_READING", "FINISHED", "WANTS_TO_READ"];
+  };
 /**
  * Get the reading section heading based on the status
  * @param status {LiteralReadingStatus} - the reading status
  * @returns {string} - the reading section heading
  */
-export const getReadingSectionHeading = (status: LiteralReadingStatus): string => {
-    switch (status) {
-        case "IS_READING":
-            return "Books I'm reading"
-        case "FINISHED":
-            return "Books I've read"
-        case "WANTS_TO_READ":
-            return "Books I want to read"
-    }
-}
+export const getReadingSectionHeading = (
+  status: LiteralReadingStatus,
+): string => {
+  switch (status) {
+    case "IS_READING":
+      return "Books I'm reading";
+    case "FINISHED":
+      return "Books I've read";
+    case "WANTS_TO_READ":
+      return "Books I want to read";
+  }
+};
 /**
  * Get the cover image of the book or the default avatar
  * @param cover {string | undefined} - the cover image of the book
  * @returns {string} - the cover image of the book or the default avatar
  */
 export const getCoverImage = (cover?: string | undefined): string => {
-    return cover ?? FALLBACK_IMAGE;
-}
+  return cover ?? FALLBACK_IMAGE;
+};
 /**
  * Get the books based on the reading status
  * @param readingStates {LiteralReadingState[]} - the reading states
  * @param status {LiteralReadingStatus} - the reading status
  * @returns {LiteralBook[]} - the books based on the reading status
  */
-export const getBooks = (readingStates: LiteralReadingState[], status: LiteralReadingStatus): LiteralBook[] => {
-    return readingStates?.filter(state => state.status === status)?.map(state => state.book)
-}
+export const getBooks = (
+  readingStates: LiteralReadingState[],
+  status: LiteralReadingStatus,
+): LiteralBook[] => {
+  return readingStates
+    ?.filter((state) => state.status === status)
+    ?.map((state) => state.book);
+};
 /**
  * Checks if the environment is development or not
  * @returns {boolean} - true if the environment is development, false otherwise
  */
-export const isDev = (): boolean => process.env.NODE_ENV === "development"
+export const isDev = (): boolean => process.env.NODE_ENV === "development";
 
 /**
  * Get the blog posts from the substack feed based on the path of the page.
@@ -154,26 +176,45 @@ export const isDev = (): boolean => process.env.NODE_ENV === "development"
  * @param items
  * @param path {string | null} - the path of the page
  */
-export const getBlogPosts = (items?: SubstackItem[], path?: string | null) => items?.slice(0, path === "/" ? 4 : undefined)
+export const getBlogPosts = (items?: SubstackItem[], path?: string | null) =>
+  items?.slice(0, path === "/" ? 4 : undefined);
 /**
  * Sets the active class based on the current path.
  * @param currentPath {string | null} - the current path
  * @param path {string | null} - the path
  */
-export const setActiveClass = (currentPath?: string | null, path?: string | null) => {
-    if (currentPath !== path) return "";
-    return "active"
-}
+export const setActiveClass = (
+  currentPath?: string | null,
+  path?: string | null,
+) => {
+  if (currentPath !== path) return "";
+  return "active";
+};
 /**
  * Typeguard which checks whether the input object is of type InstagramError
  * @param instagramObj
  */
-export const isInstagramError = (instagramObj: any): instagramObj is InstagramError => {
-    if (typeof instagramObj !== "object") return false
-    if (instagramObj.hasOwnProperty("error")) {
-        const errorObj = instagramObj.error
-        return errorObj.hasOwnProperty("code") &&
-            errorObj.hasOwnProperty("message") && errorObj.hasOwnProperty("type") && errorObj.hasOwnProperty("fbtrace_id")
-    }
-    return false
+export const isInstagramError = (
+  instagramObj: any,
+): instagramObj is InstagramError => {
+  if (typeof instagramObj !== "object") return false;
+  if (instagramObj.hasOwnProperty("error")) {
+    const errorObj = instagramObj.error;
+    return (
+      errorObj.hasOwnProperty("code") &&
+      errorObj.hasOwnProperty("message") &&
+      errorObj.hasOwnProperty("type") &&
+      errorObj.hasOwnProperty("fbtrace_id")
+    );
+  }
+  return false;
+};
+/**
+ * Removes all HTML tags from a given string.
+ * @param input - The string containing HTML tags.
+ * @returns A plain text string with all HTML tags removed.
+ */
+export function stripHtmlTags(input?: string, defaultText?: string): string {
+  if (!input) return defaultText ?? "";
+  return input.replace(/<\/?[^>]+(>|$)/g, "");
 }
